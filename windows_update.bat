6068dd0040204fb1ec4494c79a5e097ecb527a32
The Smiths <dougpesmith@gmail.com>
2023-10-08 16:10:49 +0100

    Update windows_update.bat

--- windows_update.bat
+++ windows_update.bat
@@ -1,9 +1,9 @@
 python -m pip install --upgrade pip
 pip install -U scipy
-pip install -U matplotlib
-pip install -U PySimpleGUI
 pip install -U astropy
+pip install -U matplotlib
+pip install -U pysimplegui
 pip install -U opencv-python
-pip install -U lsq-ellipse
 pip install -U scikit-image
+pip install -U lsq-ellipse
 pip install -U pillow
