f7951b1cfdcd4e31b0133e3f3bf604ef983e1877
The Smiths <dougpesmith@gmail.com>
2024-02-25 20:59:33 +0000

    update requirements
    Use the free version of PySimpleGui 4.60.5

--- windows_setup.bat
+++ windows_setup.bat
@@ -1,7 +1,7 @@
 pip install scipy
 pip install astropy
 pip install matplotlib
-pip install pysimplegui
+pip install pysimplegui==4.60.5
 pip install opencv-python
 pip install scikit-image
 pip install lsq-ellipse
--- windows_update.bat
+++ windows_update.bat
@@ -2,7 +2,6 @@ python -m pip install --upgrade pip
 pip install -U scipy
 pip install -U astropy
 pip install -U matplotlib
-pip install -U pysimplegui
 pip install -U opencv-python
 pip install -U scikit-image
 pip install -U lsq-ellipse
